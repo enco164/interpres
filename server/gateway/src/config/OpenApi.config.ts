59d2f55ad8e1adab9d9be4fba95a7a5ebc3dd040
Uros Milenkovic <enco164@gmail.com>
2021-01-04 15:44:08 +0100

    add OpenApi

---
+++ server/gateway/src/config/OpenApi.config.ts
@@ -0,0 +1,27 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupOpenApi(app: INestApplication) {
+  let BASE_URL = process.env.BASE_URL || 'http://localhost:8090';
+
+  const options = new DocumentBuilder()
+    .setTitle('Interpres API')
+    .setDescription('Interpres API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document, {
+    explorer: true,
+    customCss: '.swagger-ui .topbar { display: auto }',
+    swaggerOptions: {
+      urls: [
+        {
+          url: BASE_URL + '/api/translation-service/v3/swagger.json',
+          name: 'translation-service',
+        },
+      ],
+    },
+  });
+}
--- server/translation/src/main.ts
+++ server/translation/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/OpenApi.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(8091);
 }
+
 bootstrap();
