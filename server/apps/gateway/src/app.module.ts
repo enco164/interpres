ca888e5abf5d25a0b328770239556aa887567d1d
Uros Milenkovic <enco164@gmail.com>
2021-02-27 11:01:35 +0100

    fix tests

--- server/apps/gateway/src/app.module.ts
+++ server/apps/gateway/src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'TRANSFER',
-        transport: Transport.TCP,
-        options: {
-          host: '127.0.0.1',
-          port: 8092,
-        },
-      },
-    ]),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/apps/gateway/src/app.service.ts
+++ server/apps/gateway/src/app.service.ts
@@ -1,13 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(
-    @Inject('TRANSFER') private readonly clientTransfer: ClientProxy,
-  ) {}
-
   getHello() {
-    return this.clientTransfer.send<string>({ cmd: 'hello' }, {});
+    return 'Hello World!';
   }
 }
