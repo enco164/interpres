e5a35eca976e7d0c2c101410fa6135805b32cc97
Uros Milenkovic <enco164@gmail.com>
2021-03-20 17:16:56 +0100

    apply theme styles

--- client/src/app-providers.tsx
+++ client/src/app-providers.tsx
@@ -2,16 +2,20 @@ import { CssBaseline } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
+import { theme } from "./config/theme";
 import { store } from "./state/store";
 import { Suspense } from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 export const AppProviders: React.FC = ({ children }) => {
   return (
-    <Suspense fallback="loading">
-      <ReduxProvider store={store}>
-        <CssBaseline />
-        <Router>{children}</Router>
-      </ReduxProvider>
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <Suspense fallback="loading">
+        <ReduxProvider store={store}>
+          <CssBaseline />
+          <Router>{children}</Router>
+        </ReduxProvider>
+      </Suspense>
+    </ThemeProvider>
   );
 };
