ee58fae89f4d968db89aa371d58730593c74db86
Uros Milenkovic <enco164@gmail.com>
2021-03-24 00:05:09 +0100

    add languages to project settings

--- client/src/features/projects/project-settings.page.tsx
+++ client/src/features/projects/project-settings.page.tsx
@@ -6,8 +6,10 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
 import { Form, Formik } from "formik";
 import React, { useCallback, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { Project } from "../../domain/project";
 import { useProjectIdParam } from "../../hooks/use-project-id-param";
@@ -17,10 +19,21 @@ import {
   selectProjectById,
   updateProject,
 } from "./projects.slice";
+import ISO6391 from "iso-639-1";
+
+const mapLngCodeToLngOption = (code: string) => ({
+  name: ISO6391.getName(code),
+  code,
+});
+
+const allLngs = ISO6391.getAllCodes().map(mapLngCodeToLngOption);
+
+console.log(allLngs);
 
 interface ProjectSettingsPageProps {}
 
 export const ProjectSettingsPage: React.FC<ProjectSettingsPageProps> = () => {
+  const { t } = useTranslation(["project-settings"]);
   const dispatch = useAppDispatch();
   const projectId = useProjectIdParam();
   const project = useSelector((state: RootState) =>
@@ -64,7 +77,7 @@ export const ProjectSettingsPage: React.FC<ProjectSettingsPageProps> = () => {
                     name="name"
                     value={formik.values.name}
                     onChange={formik.handleChange}
-                    label={"Name"}
+                    label={t("formLabels.name")}
                   />
                 </Grid>
                 <Grid item xs={6} container direction="column">
@@ -73,7 +86,7 @@ export const ProjectSettingsPage: React.FC<ProjectSettingsPageProps> = () => {
                     name="githubOwner"
                     value={formik.values.githubOwner}
                     onChange={formik.handleChange}
-                    label={"Owner"}
+                    label={t("formLabels.owner")}
                   />
                 </Grid>
                 <Grid item xs={6} container direction="column">
@@ -82,7 +95,7 @@ export const ProjectSettingsPage: React.FC<ProjectSettingsPageProps> = () => {
                     name="githubRepo"
                     value={formik.values.githubRepo}
                     onChange={formik.handleChange}
-                    label={"Repo"}
+                    label={t("formLabels.repo")}
                   />
                 </Grid>
                 <Grid item xs={12} container direction="column">
@@ -91,13 +104,46 @@ export const ProjectSettingsPage: React.FC<ProjectSettingsPageProps> = () => {
                     name="lngLoadPath"
                     value={formik.values.lngLoadPath}
                     onChange={formik.handleChange}
-                    label={"Lng Load Path"}
+                    label={t("formLabels.lngLoadPath")}
+                  />
+                </Grid>
+                <Grid item xs={12} container direction="column">
+                  <Autocomplete
+                    multiple
+                    autoComplete
+                    autoHighlight
+                    autoSelect
+                    clearOnBlur
+                    options={allLngs}
+                    getOptionLabel={(option) =>
+                      `${option.name} (${option.code})`
+                    }
+                    defaultValue={[]}
+                    filterSelectedOptions
+                    onChange={(event, value) => {
+                      formik.setFieldValue(
+                        "languages",
+                        value?.map((opt) => opt.code),
+                        true
+                      );
+                    }}
+                    value={
+                      formik.values.languages?.map(mapLngCodeToLngOption) || []
+                    }
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        name="languages"
+                        variant="standard"
+                        label={t("formLabels.languages")}
+                      />
+                    )}
                   />
                 </Grid>
               </Grid>
-              <Box p={2}>
+              <Box p={2} textAlign="center">
                 <Button type="submit" variant="contained" color="primary">
-                  Save
+                  {t("formLabels.saveBtn")}
                 </Button>
               </Box>
             </Form>
